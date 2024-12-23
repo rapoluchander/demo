36349d59a5781aecd6bd5af0dc051ea82c0b8621
paripoorna.rapolu <paripoorna.rapolu@gmail.com>
2024-12-23 20:10:02 +0530

    added Service class

--- src/main/java/com/jenkins/demo/controller/DemoController.java
+++ src/main/java/com/jenkins/demo/controller/DemoController.java
@@ -1,13 +1,18 @@
 package com.jenkins.demo.controller;
 
+import com.jenkins.demo.service.DemoService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class DemoController {
 
+    @Autowired
+    public DemoService demoService;
+
     @GetMapping("/getname")
     public String getName(){
-        return "hellow Paripoorna";
+        return "Welcome "+demoService.getName();
     }
 }
---
+++ src/main/java/com/jenkins/demo/service/DemoService.java
@@ -0,0 +1,11 @@
+package com.jenkins.demo.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class DemoService {
+
+    public String getName(){
+        return "Rapolu Paripuurna Chander";
+    }
+}
